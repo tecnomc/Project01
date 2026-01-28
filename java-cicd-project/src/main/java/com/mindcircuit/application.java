8f5bc226eb22871a502404d493741b0be45897fc
shilpa <shilpa@gmail.com>
2026-01-29 00:33:03 +0530

    Application

--- java-cicd-project/src/main/java/com/mindcircuit/application.java
+++ java-cicd-project/src/main/java/com/mindcircuit/application.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class application {
+public class Application {
 
     @GetMapping("/")
     public String index(final Model model) {
@@ -18,7 +18,10 @@ public class application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
+
+
+
